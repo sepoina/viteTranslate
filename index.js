850bd09fa9a710ceaf8f71b47e7241513bea1c3c
sepoina <sepoina@gmail.com>
2023-12-19 21:50:58 +0100

    prima prova

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+function funzioneUno() {
+    console.log("uno");
+    // Implementa la tua logica qui
+  }
+  
+  function funzioneDue() {
+    console.log("due");
+    // Implementa la tua logica qui
+  }
+  
+  export default {
+    funzioneUno,
+    funzioneDue,
+  };
