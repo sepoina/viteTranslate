2a1cbcd2f6ef1febd80cc6016b1b5af91110106e
Giancarlo Ghigi <sepoina@gmail.com>
2023-12-20 16:09:13 +0100

    prime librerie

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-// index.js
-function funzioneUno() {
-    console.log("uno");
-    // Implementa la tua logica qui
-  }
-  
-  function funzioneDue() {
-    console.log("due");
-    // Implementa la tua logica qui
-  }
-  
-  export default {
-    funzioneUno,
-    funzioneDue,
-  };
+import BabelTranslate from "./babel/BabelTranslate";
+import RollupTranslate from "./rollup/RollupTranslate";
+
+export {
+  BabelTranslate,
+  RollupTranslate,
+};
