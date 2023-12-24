11b59b37a7bcc15c13b0e5dca45121861f6c865b
sepoina <sepoina@gmail.com>
2023-12-24 01:57:05 +0100

    es6

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import babelTranslate from "./dev/babel/babelTranslate";
 import rollupTranslate from "./dev/rollup/rollupTranslate";
-import TranslateContainer from "./components/TranslateContainer";
-import { TranslateContext } from "./components/TranslateContainer";
-import Translate from "./components/Translate";
 
 export {
   babelTranslate,
-  rollupTranslate,
-  TranslateContainer,
-  TranslateContext,
-  Translate,
+  rollupTranslate
 };
